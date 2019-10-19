c950e05fd54c62cdcab8ba1253032e5ee78d5ad1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-19 06:51:59 +0000

    Deploy ParkourKarthik/tic-tac-toe to github.com/ParkourKarthik/tic-tac-toe.git:gh-pages

---
+++ precache-manifest.cef513b1a99598e5639a4a3cd4b59fb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1445c8f920b2102e038c7ea539f0f8f6",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f6d347df7e24880f2196",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "0c4bf59f1a6fbfb240a1",
+    "url": "/tic-tac-toe/static/js/2.d18ffbff.chunk.js"
+  },
+  {
+    "revision": "f6d347df7e24880f2196",
+    "url": "/tic-tac-toe/static/js/main.31f60870.chunk.js"
+  },
+  {
+    "revision": "b2217611b64eeb1598e6",
+    "url": "/tic-tac-toe/static/js/runtime-main.c7edb049.js"
+  }
+]);
